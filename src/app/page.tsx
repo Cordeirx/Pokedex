1541b313e17f26efbc581b39f96395a850d27df6
Eric Cordeiro <lele.eric.santos@gmail.com>
2025-04-28 15:29:39 -0300

    att final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   useEffect(() => {
     async function fetchPokemons() {
       try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=12');
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
         const data = await response.json();
 
         const detailedPokemons = await Promise.all(
